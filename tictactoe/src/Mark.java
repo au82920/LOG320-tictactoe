20ec6edb189b8d2f1232eb013a762bec3fff8d70
au82920 <mikael.lafitte-groulx.1@ens.etsmtl.ca>
2026-06-06 22:26:11 -0400

    Revert "Mikael"

--- tictactoe/src/Mark.java
+++ tictactoe/src/Mark.java
@@ -1,16 +1,7 @@
 
-enum Mark {
-    X,
-    O,
-    EMPTY;
-
-    public Mark getOpposite() {
-        if (this == Mark.O) {
-            return Mark.X;
-        }
-        if (this == Mark.X) {
-            return Mark.O;
-        }
-        return Mark.EMPTY;
+enum Mark{
+        X,
+        O,
+        EMPTY
     }
-}
+
